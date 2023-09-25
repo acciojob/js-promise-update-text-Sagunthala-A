a44f29f46ab867a712f8ecfa48df12b23fdda2b8
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2023-09-25 18:45:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+const output = document.getElementById("output");
+
+async function delayedHello() {
+            return await new Promise((resolve,reject) => {
+                setTimeout(() => {
+                    resolve("Hello, world!");
+                }, 1000);
+            });
+        }
+
+delayedHello().then((data) => {
+const para = document.createElement("p");
+para.innerText = data
+
+output.appendChild(para);
+    
+})
